b41548a8c82ea6651c47457451e7591dbd1f5aec
8achi <shibaprasadpatra1999@gmail.com>
2022-04-13 20:26:59 +0530

    Add About page also modified some features

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 var items = ["Buy Food", "Cook Food", "Eat Food",];
+var workItems = [];
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-  var options = {
+  const options = {
     weekday: 'long',
     year: 'numeric',
     month: 'long',
@@ -17,15 +18,35 @@ app.get("/", function(req, res) {
   var date = today.toLocaleDateString("en-US", options);
 
   res.render("list", {
-    kindOfDay: date,
+    listTitle: date,
     newItem: items,
   });
 });
 
 app.post('/', function(req, res) {
-  item = req.body.todo;
+let  item = req.body.todo;
+
+if(req.body.list === "Work"){
+  workItems.push(item);
+  res.redirect('/work');
+}
+
+else{
   items.push(item);
   res.redirect("/");
+}
+
+});
+
+app.get('/work', function(req, res) {
+  res.render('list',{
+    listTitle : "Work List",
+    newItem : workItems,
+  });
+});
+
+app.get('/about', function(req, res) {
+  res.render('about',);
 });
 
 app.listen("3000", function() {
